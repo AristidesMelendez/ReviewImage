8844a2681e84c44a881114bb56dcfb8c441b6808
AristidesMelendez <aristides.melendez@admios-sa.com>
2015-01-14 16:49:07 -0500

    Add logic to change image when it's approved or declined

--- ReviewImage.js
+++ ReviewImage.js
@@ -11,21 +11,59 @@ var reviewImage = angular.module('reviewImage', []);
 
 reviewImage.controller('ReviewController', function(){
 	
-	this.image = {
-		stars : '5',
-		description : 'Add a description here.',
-		clientId : 'aristides'
-	};
+	var count = 0;
+	var parent = this;
+
+	this.images = [
+		{
+			stars : '5',
+			description : 'Add a description here.',
+			clientId : 'aristides',
+			url: 'http://dummyimage.com/200x200/000/fff',
+			title: 'image 1'
+		},
+		{
+			stars : '3',
+			description : 'Add a description here.',
+			clientId : 'persona2',
+			url: 'http://dummyimage.com/200x200/e90/00f/',
+			title: 'imagen 2'
+		},
+		{
+			stars : '4',
+			description : 'Add a description here.',
+			clientId : 'persona3',
+			url: 'http://dummyimage.com/200x200/00d/000',
+			title: 'immagine 3'
+		}
+	];
+
+	this.image = this.images[count];
 	
 	this.accept = function(image){
-		var bla;
-		
 		image.accepted = true;
+		this.nextImage();
 	};
 	
 	this.denied = function(image){
-		var blo;
-		
 		image.accepted = false;
+		this.nextImage();
+	};
+
+	this.nextImage = function(){
+		console.log('xopa ' + count);
+		console.log('images length: ' + this.images.length);
+
+		if(count < (this.images.length - 1)){
+			this.image =  this.images[++count];
+		} else {
+			// last Image reach.
+			this.image = {
+				title: 'End of images',
+				url: 'http://dummyimage.com/200x200/0f0/f0f',
+				
+			}
+		}
+
 	};
 });
