933b56c76f51211c94a27fa21e75c99e1b0b78e3
AristidesMelendez <aristides.melendez@admios-sa.com>
2015-01-14 14:24:15 -0500

    Creating initial module

---
+++ ReviewImage.js
@@ -0,0 +1,11 @@
+// Demo application to learn AngularJS
+// Author: Aristides Melendez
+// Date: 2015/01/14 11:22 -0500 GTM
+
+
+'use strict';
+
+/* App Module */
+
+var reviewImage = angular.module('reviewImage', []);
+
