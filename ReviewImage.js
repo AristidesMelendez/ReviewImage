e4c5781e8d55fb0b1ffbce63e44d4bca8520f42d
AristidesMelendez <aristides.melendez@admios-sa.com>
2015-01-15 17:03:38 -0500

    Adding dependency to use web service from flickr

--- ReviewImage.js
+++ ReviewImage.js
@@ -9,37 +9,75 @@
 
 var reviewImage = angular.module('reviewImage', []);
 
-reviewImage.controller('ReviewController', function(){
+var reviewController = function($http){
 	
 	var count = 0;
-	var parent = this;
+	this.images = [];
+	var loadedImages = this.images;
 
-	this.images = [
-		{
-			stars : '5',
-			description : 'Add a description here.',
-			clientId : 'aristides',
-			url: 'http://dummyimage.com/200x200/000/fff',
-			title: 'image 1'
-		},
-		{
-			stars : '3',
-			description : 'Add a description here.',
-			clientId : 'persona2',
-			url: 'http://dummyimage.com/200x200/e90/00f/',
-			title: 'imagen 2'
-		},
-		{
-			stars : '4',
-			description : 'Add a description here.',
-			clientId : 'persona3',
-			url: 'http://dummyimage.com/200x200/00d/000',
-			title: 'immagine 3'
-		}
-	];
+	var fetchImages = function ($http){
 
+		var url = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=17592ab295bfffea2dbfac2566bb9cd0&per_page=20&format=json&jsoncallback=success',
+		    argumentos = { };
+
+		$http.get(url, argumentos)
+		.success(function (data) {
+			var results = data.substring(8,data.length);
+			results = results.substring(0, results.length-1);
+			results = JSON.parse(results);
+			console.log(data.photos);
+
+			var photo;
+			for (var i = results.photos.photo.length - 1; i >= 0; i--) {
+				photo = {
+					description: '',
+					clientId: results.photos.photo[i].owner,
+					title: results.photos.photo[i].title,
+					url: 'https://farm' + results.photos.photo[i].farm + '.staticflickr.com/' + results.photos.photo[i].server + '/' + results.photos.photo[i].id + '_' + results.photos.photo[i].secret + '_n.jpg'
+				};
+
+				
+				loadedImages.push(photo);
+			};
+
+			//loadedImages = data;
+			return;
+		})
+		.error(function (data){
+			console.log('error in get call.');
+		});
+	};
+
+	
+	
+	fetchImages($http);
 	this.image = this.images[count];
 	
+	//this.images = [
+	// 	{
+	// 		stars : '5',
+	// 		description : 'Add a description here.',
+	// 		clientId : 'aristides',
+	// 		url: 'http://dummyimage.com/200x200/000/fff',
+	// 		title: 'image 1'
+	// 	},
+	// 	{
+	// 		stars : '3',
+	// 		description : 'Add a description here.',
+	// 		clientId : 'persona2',
+	// 		url: 'http://dummyimage.com/200x200/e90/00f/',
+	// 		title: 'imagen 2'
+	// 	},
+	// 	{
+	// 		stars : '4',
+	// 		description : 'Add a description here.',
+	// 		clientId : 'persona3',
+	// 		url: 'http://dummyimage.com/200x200/00d/000',
+	// 		title: 'immagine 3'
+	// 	}
+	// ];
+
+
 	this.accept = function(image){
 		image.accepted = true;
 		this.nextImage();
@@ -51,9 +89,7 @@ reviewImage.controller('ReviewController', function(){
 	};
 
 	this.nextImage = function(){
-		console.log('xopa ' + count);
-		console.log('images length: ' + this.images.length);
-
+		
 		if(count < (this.images.length - 1)){
 			this.image =  this.images[++count];
 		} else {
@@ -66,13 +102,18 @@ reviewImage.controller('ReviewController', function(){
 		}
 
 	};
-});
+
+};
+
+
+reviewImage.controller('ReviewController', ['$http',reviewController]);
 
 reviewImage.directive('imageViewForm', function(){
 	return {
 		retrict: 'E',
-		templateUrl: 'imageViewForm.html'
-		//controller: ReviewController()
+		templateUrl: 'imageViewForm.html',
+		controller: reviewController,
+		controllerAs: 'reviewCtrl'
 
 	};
 
