3df8ac37f377795edcd5d8bf1298e9a529cf9252
AristidesMelendez <aristides.melendez@admios-sa.com>
2015-01-21 13:43:53 -0500

    add structure to app.

---
+++ js/controllers.js
@@ -0,0 +1,69 @@
+'use strict';
+
+//Controllers
+var reviewControllers = angular.module('reviewControllers',[]);
+
+reviewControllers.controller('reviewController', ['$http', function($http){
+	
+	var count = 0;
+	var review = this;
+	var loadedImages = [];
+	this.hasPhotos = false;
+
+	var fetchImages = function ($http){
+
+		var url = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=17592ab295bfffea2dbfac2566bb9cd0&per_page=20&format=json&nojsoncallback=1';
+
+		$http.get(url)
+		.success(function (data) {
+
+			var photo;
+			for (var i = data.photos.photo.length - 1; i >= 0; i--) {
+				var rootUrl = 'https://farm' + data.photos.photo[i].farm + '.staticflickr.com/' + data.photos.photo[i].server + '/' + data.photos.photo[i].id + '_' + data.photos.photo[i].secret;
+				photo = {
+					description: '',
+					clientId: data.photos.photo[i].owner,
+					title: data.photos.photo[i].title,
+					url: rootUrl + '_z.jpg',
+					thumbnail: rootUrl + '_q.jpg',
+					target: 'https://www.flickr.com/photos/' + data.photos.photo[i].owner + '/' + data.photos.photo[i].id
+				};
+
+				loadedImages.push(photo);
+			}
+
+			console.log('loadedImages: ' + loadedImages.length);
+			review.image = loadedImages[0];
+			review.hasPhotos = true;
+		})
+		.error(function (data){
+			console.log('error in get call.');
+		});
+	};
+	
+	fetchImages($http);
+
+	this.accept = function(image){
+		image.accepted = true;
+		this.nextImage();
+	};
+	
+	this.denied = function(image){
+		image.accepted = false;
+		this.nextImage();
+	};
+
+	this.nextImage = function(){
+		if(count < (loadedImages.length - 1)){
+			this.image =  loadedImages[++count];
+		} else {
+			review.hasPhotos = false;
+			// last Image reach.
+			this.image = {
+				title: 'End of images',
+				url: 'http://dummyimage.com/200x200/0f0/f0f'
+			}
+		}
+	};
+}]);
+
