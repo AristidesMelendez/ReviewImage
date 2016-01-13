9a30881f56a77e2166cbdaf4ade6d80acda1a9ba
AristidesMelendez <aristides.melendez@admios-sa.com>
2016-01-13 11:25:18 -0500

    Finish end2end test.

--- tests/spec.js
+++ tests/spec.js
@@ -8,7 +8,6 @@ describe('Review image App', function() {
     browser.get('https://aristidesmelendez.github.io/');
 
     expect(browser.getTitle()).toEqual('Review Image');
-    expect(element(by.css("div.noPhotos h1")).getWebElement().getText()).toEqual('');
 
     // 1st photo
     expect(element(by.css(".container div.page-header h1.col-md-12")).getWebElement().getText()).not.toEqual('No more photos');
@@ -22,20 +21,51 @@ describe('Review image App', function() {
 
     // 2nd photo
     expect(element(by.css(".container div.page-header h1.col-md-12")).getWebElement().getText()).not.toEqual('No more photos');
-    element(by.model('image.description')).sendKeys('algun comentario para esta image.');
-    element(by.model('image.stars')).sendKeys(3);
+    element(by.model('image.description')).sendKeys('No me gusto');
+    element(by.model('image.stars')).sendKeys(1);
     expect(element(by.css('button.btn-danger')).getText()).toEqual('Reject');
     expect(element(by.css('button.btn-success')).getText()).toEqual('Accept');
-    element(by.css('button.btn-success')).click();
+    element(by.css('button.btn-danger')).click();
 
     // 3rd photo
     expect(element(by.css(".container div.page-header h1.col-md-12")).getWebElement().getText()).not.toEqual('No more photos');
-    element(by.model('image.description')).sendKeys('algun comentario para esta image.');
-    element(by.model('image.stars')).sendKeys(3);
+    element(by.model('image.description')).sendKeys('Aprobada.');
+    element(by.model('image.stars')).sendKeys(5);
     expect(element(by.css('button.btn-danger')).getText()).toEqual('Reject');
     expect(element(by.css('button.btn-success')).getText()).toEqual('Accept');
     element(by.css('button.btn-success')).click();
 
+    expect(element(by.css("div.noPhotos h1")).getWebElement().getText()).toEqual('No more photos');
+    element(by.css('div.noPhotos a')).click();
+
+    expect(element(by.css("div.page-header h1")).getWebElement().getText()).toEqual('Report');
+    var checkedItems = element.all(by.css('div.col-md-4'));
+    expect(checkedItems.count()).toEqual(3);
+
+    //using a then() to sort in items.
+    element.all(by.css('div.col-md-4')).then(function (checkedItems) {
+        expect(checkedItems.length).toBe(3);
+
+	    checkedItems[0].all(by.css('div')).then( function (imageInfo) {
+	        expect(imageInfo[1].element(by.css('label')).getText()).toEqual('Accepted: true');
+	        expect(imageInfo[2].element(by.css('label')).getText()).toEqual('Description: algun comentario para esta image.');
+	        expect(imageInfo[3].element(by.css('label')).getText()).toEqual('Stars: 3');
+	    });
+
+	    checkedItems[1].all(by.css('div')).then( function (imageInfo) {
+	        expect(imageInfo[1].element(by.css('label')).getText()).toEqual('Accepted: false');
+	        expect(imageInfo[2].element(by.css('label')).getText()).toEqual('Description: No me gusto');
+	        expect(imageInfo[3].element(by.css('label')).getText()).toEqual('Stars: 1');
+	    });
+
+	    checkedItems[2].all(by.css('div')).then( function (imageInfo) {
+	        expect(imageInfo[1].element(by.css('label')).getText()).toEqual('Accepted: true');
+	        expect(imageInfo[2].element(by.css('label')).getText()).toEqual('Description: Aprobada.');
+	        expect(imageInfo[3].element(by.css('label')).getText()).toEqual('Stars: 5');
+	    });
+
+    });
 
   });
+
 });
